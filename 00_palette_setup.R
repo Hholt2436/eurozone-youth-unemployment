5930f3287d733e9cf5d7a33f72c7ee84514c6eff
Henry Holt <henryholt@Henrys-MacBook-Air.local>
2026-06-02 16:38:42 +0200

    Design: finalize country-mapped editorial palette vectors

---
+++ 00_palette_setup.R
@@ -0,0 +1,10 @@
+fed_typography <- "#334B5B"
+fed_core_blue <- "#62AEE1"
+fed_core_gold <- "#B1812C"
+fed_core_gray <- "#9FA1A8"
+fed_spain_red <- "#B84545"
+
+my_fed_palette <- c(fed_core_blue, 
+                    fed_core_gold, 
+                    fed_core_gray, 
+                    fed_spain_red)
