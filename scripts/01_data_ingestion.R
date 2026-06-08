e2b21bdbb8bae5eb39bc438d5c40368479f18ed9
Henry Holt <henryholt@Henrys-MacBook-Air.local>
2026-06-08 16:27:43 +0200

    data: build initial API pipeline for quarterly GDP and youth unemployment

---
+++ scripts/01_data_ingestion.R
@@ -0,0 +1,44 @@
+#Harmonized Quarterly Youth Unemployment Rate (Outcome Variable: Y)
+target_series <- tibble(
+  series_id = c("LRUN24TTESQ156S", 
+                "LRUN24TTDEQ156S", 
+                "LRUN24TTFRQ156S", 
+                "LRUN24TTITQ156S"),
+  country_code = c("ESP", "DEU", 
+                   "FRA", "ITA" 
+                   ),
+  region = c("Periphery", "Core", 
+             "Core", "Periphery")
+)
+raw_youth_unemp <- 
+  target_series$series_id |> 
+  map_df(function(id) {
+    fredr(
+      series_id = id,
+      observation_start = as.Date("1995-01-01"),
+      observation_end   = as.Date("2025-12-31")
+    )
+  }) |> 
+  left_join(target_series, by = "series_id")
+#Real GDP (Exogenous Confounder: X)
+target_series_2 <- tibble(
+  series_id = c("CLVMNACSCAB1GQES", 
+                "CLVMNACSCAB1GQDE", 
+                "CLVMNACSCAB1GQFR", 
+                "CLVMNACSCAB1GQIT"),
+  country_code = c("ESP", "DEU", 
+                   "FRA", "ITA" 
+  ),
+  region = c("Periphery", "Core", 
+             "Core", "Periphery")
+)
+raw_gdp_growth <- 
+  target_series_2$series_id |> 
+  map_df(function(id) {
+    fredr(
+      series_id = id,
+      observation_start = as.Date("1995-01-01"),
+      observation_end   = as.Date("2025-12-31")
+    )
+  }) |> 
+  left_join(target_series_2, by = "series_id")
