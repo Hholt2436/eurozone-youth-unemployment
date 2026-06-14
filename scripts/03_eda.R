de1428113995e7889a5e212f1e7ac2073179b2d0
Henry Holt <henryholt@Henrys-MacBook-Air.local>
2026-06-14 14:48:55 +0200

    EDA baseline: wide pivot, epl_gap, zero-NA guardrail, sessionInfo snapshot

--- scripts/03_eda.R
+++ scripts/03_eda.R
@@ -1,5 +1,12 @@
+library(tidyverse)
+library(lubridate)
+library(fredr)
+library(countrycode)
 eurozone_macro_wide <- eurozone_macro_clean |>
   pivot_wider(names_from = Measure, values_from = epl_index) |>
   rename(epl_regular = `Individual dismissals (regular contracts)`,
          epl_temporary = `Temporary contracts`) |>
   mutate(epl_gap = epl_regular - epl_temporary)
+
+stopifnot(sum(is.na(eurozone_macro_wide)) == 0)
+  
